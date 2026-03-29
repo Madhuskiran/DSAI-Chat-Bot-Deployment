f31f1d15085c9fa7494a0edfcd447da6d3818b39
Madhuskiran <132285399+Madhuskiran@users.noreply.github.com>
2026-03-29 13:18:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from langchain_groq import ChatGroq
+
+# Load API key
+api_key = st.secrets["GROQ_API_KEY"]
+
+st.title("RAG Chatbot with Groq")
+
+# Upload PDF
+uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
+
+if uploaded_file:
+    
+    # Save file temporarily
+    with open("temp.pdf", "wb") as f:
+        f.write(uploaded_file.read())
+
+    # Load PDF
+    loader = PyPDFLoader("temp.pdf")
+    documents = loader.load()
+
+    # Split text
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+    chunks = text_splitter.split_documents(documents)
+
+    # Embeddings
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    # Vector store
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
+    # Groq LLM
+    llm = ChatGroq(
+        groq_api_key=api_key,
+        model_name="llama-3.1-8b-instant"
+    )
+
+    # User input
+    question = st.text_input("Ask a question")
+
+    if st.button("Ask"):
+        docs = retriever.invoke(question)
+        context = "\n\n".join(doc.page_content for doc in docs)
+
+        prompt = f"""
+        Use ONLY the context below to answer the question.
+        If not found, say "Not available in document."
+
+        Context:
+        {context}
+
+        Question:
+        {question}
+
+        Answer:
+        """
+
+        response = llm.invoke(prompt)
+
+        st.success(response.content)
